0a0a2495137aabe5e94bab64e3ac0bb5c79b24b6
Arron Moore <arron.moore93@gmail.com>
2021-03-14 16:31:15 +0000

    Added working blackbox, added events for detecting session and lap changes

--- py_blackbox/api/game.py
+++ py_blackbox/api/game.py
@@ -1,8 +1,21 @@
 from lib.s_page_file_graphic import SPageFileGraphic
+from ctypes import sizeof
+import mmap
+
+
+class GraphicWrapper:
+    def __getattr__(self, attr):
+        buf = mmap.mmap(-1, sizeof(SPageFileGraphic), u"Local\\acpmf_graphics")
+        data = SPageFileGraphic.from_buffer(buf)
+        return getattr(data, attr)
+
 
 class GameApi():
     def __init__(self):
-        self.graphic = SPageFileGraphic()
+        self.graphic = GraphicWrapper()
 
     def get_session_status(self):
         return self.graphic.AC_STATUS
+
+    def get_number_of_laps(self):
+        return self.graphic.completedLaps
--- py_blackbox/main.py
+++ py_blackbox/main.py
@@ -1,23 +1,26 @@
 import time
+import mmap
+
+from ctypes import Structure, sizeof, c_float, c_int32, c_wchar, c_int
 
 from poller.poller import Poller
 from observer.observer import Observer
 from observer.event import Event
 from api.game import GameApi
+from lib.s_page_file_graphic import SPageFileGraphic
 
 
 class Game(Observer):
     def __init__(self):
-        print("Room is ready.")
-        Observer.__init__(self) # Observer's init needs to be called
+        Observer.__init__(self)  # Observer's init needs to be called
 
     def session_changed(self, data=None):
-        print("session changed")
+        print(time.time(), "session changed")
         # create a new session in the backend
         # get a sessionId - assign it to class
-    
-    def on_new_lap(self, data=None):
-        print("on new lap")
+
+    def on_new_lap(self, data):
+        print(time.time(), "on new lap", data)
         # register previous lap data against session id in
         # the backend
 
@@ -28,18 +31,36 @@ def session_event_loop():
 
     while True:
         last_session_status = session_status
-        session_status = api.get_status_session()
+        session_status = api.get_session_status()
+
+        print(session_status)
         if last_session_status != session_status:
             Event("onSessionChange")
         time.sleep(1)
 
+
+def lap_event_loop():
+    api = GameApi()
+    current_lap = api.get_number_of_laps()
+
+    while True:
+        last_lap = current_lap
+        current_lap = api.get_number_of_laps()
+        if last_lap != current_lap:
+            Event("onNewLap", current_lap)
+        time.sleep(1)
+
+
 def main():
     game = Game()
     game.attach('onSessionChange',  game.session_changed)
-    p = Poller(session_event_loop)
-
+    game.attach('onNewLap',  game.on_new_lap)
+    # p = Poller(session_event_loop)
+    # api = GameApi()
     while True:
-        time.sleep(1)
+        lap_event_loop()
+        # print(api.get_number_of_laps())
+        # time.sleep(1)
 
 
 if __name__ == "__main__":
