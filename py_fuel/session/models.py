a434659f2b2b17708c4277948850adb73b55f120
Arron Moore <arron.moore93@gmail.com>
2021-03-15 13:42:33 +0000

    Added timestamps to session and laps

--- py_fuel/session/models.py
+++ py_fuel/session/models.py
@@ -5,6 +5,8 @@ from circuit.models import Circuit
 from car.models import Car
 
 class Session(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
     type = models.CharField(max_length=50)
     user = models.ForeignKey(User, on_delete=models.DO_NOTHING)    
     circuit = models.ForeignKey(Circuit, on_delete=models.DO_NOTHING)
@@ -12,6 +14,8 @@ class Session(models.Model):
 
 
 class Lap(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
     number = models.IntegerField()
     time = models.IntegerField()
     session_id = models.ForeignKey(Session, on_delete=models.CASCADE)
