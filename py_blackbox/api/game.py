64ebf9cb5b864736fe52bb9085ba386f9a12192e
Arron Moore <arron.moore93@gmail.com>
2021-03-15 00:00:18 +0000

    send type properly

--- py_blackbox/api/game.py
+++ py_blackbox/api/game.py
@@ -3,10 +3,22 @@ from lib.s_page_file_static import SPageFileStatic
 from ctypes import sizeof
 import mmap
 
+SESSION_TYPE_MAP = {
+    "-1": "Unknown"
+    "0": "Practice" ,
+    "1": "Qualifying",
+    "2": "Race",
+    "3": "Hotlap",
+    "4": "Time Attack" ,
+    "5": "Drift",
+    "6": "Drag",
+    "7": "Hot Stint",
+    "8": "Super Pole",
+}
 
 class GraphicWrapper:
     def __getattr__(self, attr):
-        buf = mmap.mmap(-1, sizeof(SPageFileGraphic), u"Local\\acpmf_graphics")
+        = mmap.mmap(-1, sizeof(SPageFileGraphic), u"Local\\acpmf_graphics): buf 
         data = SPageFileGraphic.from_buffer(buf)
         return getattr(data, attr)
 
@@ -31,7 +43,8 @@ class GameApi():
 
     def get_session_details(self):
         return {
-            "type": "practice",  # self.graphic.ACC_SESSION_TYPE,
+            "type": SESSION_TYPE_MAP[
+                str(self.graphic.ACC_SESSION_TYPE)],
             "car": self.static.carModel,
             # # silverstone is "Silverstone" can't be doing with that
             "circuit": self.static.track.lower()
