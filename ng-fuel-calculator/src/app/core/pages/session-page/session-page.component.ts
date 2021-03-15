ce55f4ab6a96be3dc1c58e8bd76b2524019b440a
Arron Moore <arron.moore93@gmail.com>
2021-03-15 00:23:05 +0000

    format laptimes

--- ng-fuel-calculator/src/app/core/pages/session-page/session-page.component.ts
+++ ng-fuel-calculator/src/app/core/pages/session-page/session-page.component.ts
@@ -24,4 +24,17 @@ export class SessionPageComponent implements OnInit {
     })
   }
 
+  convertTime(time) {
+    const ms = time % 1000;
+    time = (time - ms) / 1000;
+    const seconds = time % 60;
+    time = (time - seconds) / 60;
+    const mins = time % 60;
+    console.log(mins)
+    const minsPadded = mins.toString().padStart(2, "0")
+    const secondsPadded = seconds.toString().padStart(2, "0")
+    const msPadded = ms.toString().padStart(3, "0")
+    return `${minsPadded}:${secondsPadded}:${msPadded}`;
+  }
+
 }
