1302ee90dbbd5302d8a90b89f6403ad8aa3f7807
Arron Moore <arron.moore93@gmail.com>
2021-03-24 22:20:52 +0000

    use live site for recordings

--- py_blackbox/config.py
+++ py_blackbox/config.py
@@ -1,6 +1,3 @@
 import os
 
-API_ENDPOINT = "http://192.168.0.143:8000/"
-
-if os.environ.get("ENVIRONMENT") == "production":
-    API_ENDPOINT = "http://fuel.arron.id/"
+API_ENDPOINT = "http://blackbox.arron.id/"
