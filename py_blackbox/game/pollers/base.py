0b2836e1c444a3e568e3c6e1980a5039b4232f2d
Arron Moore <arron.moore93@gmail.com>
2021-03-28 15:19:49 +0100

    removed obsolete base file

--- py_blackbox/game/pollers/base.py
+++
@@ -1,9 +0,0 @@
-import threading
-import time
-
-
-class BackgroundEventLoop:
-    def __init__(self, fn):
-        self.thread = threading.Thread(target=fn)
-        self.thread.daemon = True
-        self.thread.start()
