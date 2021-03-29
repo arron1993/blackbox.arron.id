f00c92e7554a3e4cedd447e83c98c67e806e9489
Arron Moore <arron.moore93@gmail.com>
2021-03-29 15:00:32 +0100

    Run migrations in docker file

--- py_blackbox_backend/car/models.py
+++ py_blackbox_backend/car/models.py
@@ -6,3 +6,4 @@ from django.db import models
 class Car(models.Model):
     name = models.CharField(max_length=100)
     keyname = models.CharField(max_length=100)
+    group = models.CharField(max_length=10)
