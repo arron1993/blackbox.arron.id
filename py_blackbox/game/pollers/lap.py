8560eb78b536fd9914746676ecb82a5abea20ad7
Arron Moore <arron.moore93@gmail.com>
2021-03-31 22:57:47 +0100

    attempt to improve new lap detection

--- py_blackbox/game/pollers/lap.py
+++ py_blackbox/game/pollers/lap.py
@@ -16,8 +16,12 @@ class LapPoller():
         self.thread.daemon = True
         self.thread.start()
 
+    def _has_lap_changed(self, last_lap):
+        return self.api.get_completed_laps() != last_lap
+
     def run(self):
         current_sector = self.api.get_current_sector()
+        current_lap = self.api.get_completed_laps()
         while True:
             try:
                 # to track a lap change we detect when
@@ -25,11 +29,27 @@ class LapPoller():
                 # can record the sector times
                 # then create the lap entry
                 if self.api.get_session_status() == SessionStatus.LIVE.value:
+                    last_lap = current_lap
+                    current_lap = self.api.get_completed_laps()
                     # no point doing work if the game isn't running
                     last_sector = current_sector
                     current_sector = self.api.get_current_sector()
+
                     if current_sector != last_sector:
-                        Event("onNewSector", last_sector)
+                        if last_sector == 2 and not self._has_lap_changed(last_lap):
+                            # sometimes we cross the last sector
+                            # but when we get the lap the data its for
+                            # the last last lap
+                            # so retry here until we're sure
+                            # we're on the next lap
+                            for _ in range(5):
+                                if self._has_lap_changed(last_lap):
+                                    Event("onNewSector", last_sector)
+                                    break
+                                else:
+                                    time.sleep(1)
+                        else:
+                            Event("onNewSector", last_sector)
             except Exception as e:
                 print(datetime.datetime.now(), "Lap Error", e)
-            time.sleep(3)
+            time.sleep(1)
