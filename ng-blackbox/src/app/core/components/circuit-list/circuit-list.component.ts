0b47734886f203796affd12df2c4c533cb886e1f
Arron Moore <arron.moore93@gmail.com>
2021-04-16 22:34:50 +0100

    save selected circuit in local storage

--- ng-blackbox/src/app/core/components/circuit-list/circuit-list.component.ts
+++ ng-blackbox/src/app/core/components/circuit-list/circuit-list.component.ts
@@ -10,14 +10,27 @@ export class CircuitListComponent implements OnInit {
   @Output() circuit = new EventEmitter();
 
   circuits = [];
+  circuitId = null;
   constructor(private cs: CircuitService) {}
 
   ngOnInit(): void {
     this.cs.get().subscribe((resp: any) => {
       this.circuits = resp;
+      this.circuitId = localStorage.getItem(`filter-circuitId`);
+      if (this.circuitId !== undefined) {
+        this.emit(this.circuitId);
+      }
     });
   }
+
+  emit(value) {
+    this.circuit.emit(value);
+  }
+
   onChange(event) {
-    this.circuit.emit(event.target.value);
+    const value = event.target.value;
+    this.circuitId = value;
+    localStorage.setItem(`filter-circuitId`, value);
+    this.emit(value);
   }
 }
