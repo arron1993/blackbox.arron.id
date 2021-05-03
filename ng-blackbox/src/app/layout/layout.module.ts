4ea470e71b305a993a1984692483dfc319c9ea16
Arron Moore <arron.moore93@gmail.com>
2021-05-03 22:33:55 +0100

    fixed links

--- ng-blackbox/src/app/layout/layout.module.ts
+++ ng-blackbox/src/app/layout/layout.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 import { SideMenuComponent } from './components/side-menu/side-menu.component';
 import { MobileMenuComponent } from './components/mobile-menu/mobile-menu.component';
 import { MobileNavbarComponent } from './components/mobile-navbar/mobile-navbar.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [SideMenuComponent, MobileMenuComponent, MobileNavbarComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   exports: [SideMenuComponent, MobileMenuComponent, MobileNavbarComponent],
 })
 export class LayoutModule {}
