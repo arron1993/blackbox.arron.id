beb10f90dbbe143ae603eb1af156188aea241def
Arron Moore <arron.moore93@gmail.com>
2021-05-03 16:27:50 +0100

    Added caching to session types

--- py_blackbox_backend/session_type/views.py
+++ py_blackbox_backend/session_type/views.py
@@ -1,3 +1,6 @@
+from django.utils.decorators import method_decorator
+from django.views.decorators.cache import cache_page
+
 from rest_framework import generics, status
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
@@ -6,7 +9,14 @@ from session_type.models import SessionType
 from session_type.serializers.session_type import SessionTypeSerializer
 
 
-class SessionTypeList(generics.ListCreateAPIView):
+class SessionTypeList(generics.ListAPIView):
     permission_classes = (IsAuthenticated,)
     serializer_class = SessionTypeSerializer
     queryset = SessionType.objects.all()
+
+    @method_decorator(cache_page(60*60*24))
+    def list(self, request):
+        # Note the use of `get_queryset()` instead of `self.queryset`
+        queryset = self.get_queryset()
+        serializer = SessionTypeSerializer(queryset, many=True)
+        return Response(serializer.data)
